11eaf902455ee75f3d6d05c45684a8dd055eb026
Divo <u24607526@tuks.co.za>
2025-09-27 19:20:08 +0200

    dogcat

---
+++ CtrlCat.h
@@ -0,0 +1,17 @@
+#ifndef DOGORITHM
+#define DOGORITHM
+#include "ChatRoom.h"
+
+class CtrlCat: public ChatRoom
+{
+    private:    
+        Users* users;
+        string* chatHistory;
+    public:
+        void registerUser(Users*);
+        void sendMessage(string,Users);
+        void saveMesssage(string,Users);
+        void removeUser(Users);
+        void clone();
+};
+#endif
