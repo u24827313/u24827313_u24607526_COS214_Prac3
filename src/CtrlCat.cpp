6660a9b27c01013a1bf5c08b9721137d0f940792
Divo <u24607526@tuks.co.za>
2025-09-28 16:37:44 +0200

    something

--- src/CtrlCat.cpp
+++ src/CtrlCat.cpp
@@ -11,22 +11,22 @@ CtrlCat::CtrlCat(Users* user,string** chatHistory):ChatRoom(user,chatHistory)
 
 void CtrlCat::registerUser(Users* user)
 {
-    
+    //user->chatRoom=this; what do you want form meeee
 }
 
 void CtrlCat::sendMessage(std::string message,Users user)
 {
-
+    user.send(message,this);
 }
 
 void CtrlCat::saveMesssage(std::string message,Users user)
 {
-
+    user.send(message,this);
 }
 
 void CtrlCat::removeUser(Users user)
 {
-
+    //idk im so tired
 }
 
 void CtrlCat::clone()
