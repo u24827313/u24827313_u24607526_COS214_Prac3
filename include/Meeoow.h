02553d367f1a912c5e14b725bc5d380f1319481b
Divo <u24607526@tuks.co.za>
2025-09-29 22:21:33 +0200

    added doxygen

--- include/Meeoow.h
+++ include/Meeoow.h
@@ -1,6 +1,22 @@
+/**
+ * @file Meeoow.h
+ * @brief Header file for the Meeoow class
+ * @defgroup MeeoowwModule Users Module
+ * @brief Module for creating a participant user to demonstrate the Users class
+ */
+
+
 #ifndef MEOW_H
 #define MEOW_H
 #include "Users.h"
+
+
+/**
+ * @class Meeoow
+ * @brief is a user participating in a chat room.
+ *
+ * meeoow class used to demonstrate how the user works practically
+ */
 
 class Meeoow: public Users{
 
