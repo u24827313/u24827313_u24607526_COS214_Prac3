a99bafa63dd94c93c99c1b6f899d398cb56810ee
Divo <u24607526@tuks.co.za>
2025-09-29 22:29:00 +0200

    i think this is the last of the doxygen

--- include/SendMessageCommand.h
+++ include/SendMessageCommand.h
@@ -15,6 +15,10 @@ class SendMessageCommand: public Command
     * @brief Log the message ent by the User(Invoker)
     */
     public:
+
+        /**
+         * @brief Receives a message from another user in a chat room and executes the command.
+        */
         void execute();
         SendMessageCommand(ChatRoom* room, string message, Users* fromUser);
 };
--- include/SillyGoose.h
+++ include/SillyGoose.h
@@ -1,6 +1,20 @@
+/**
+ * @file SillyGoose.h
+ * @brief Header file for the SillyGoose class
+ * @defgroup SIllyGooseModule Users Module
+ * @brief Module for creating a participant user to demonstrate the Users class
+ */
+
 #ifndef SILLYGOOSE_H
 #define SILLYGOOSE_H
 #include "Users.h"
+
+/**
+ * @class SillyGoose
+ * @brief is a user participating in a chat room.
+ *
+ * SillyGoose class used to demonstrate how the user works practically
+ */
 
 class SillyGoose: public Users{
 
